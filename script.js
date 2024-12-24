d4c3d16cd4a0d82257b720b89d276b15c2d3f813
misha6677 <gilcmihail31@gmail.com>
2024-12-24 13:36:18 +0300

    end

--- script.js
+++ script.js
@@ -4,45 +4,72 @@ const prevArrow = document.getElementById('prev');
 const nextArrow = document.getElementById('next');
 const dots = document.querySelectorAll('.dot');
 const slides = Array.from(document.querySelectorAll('.slide'));
+const city = document.querySelectorAll('.main_city_pacciv');
 const slideCount = slides.length;
 let slideIndex = 0;
 
 prevArrow.addEventListener('click', showPreviousSlide);
 nextArrow.addEventListener('click', showNextSlide);
 
-//делаем кнопку назад
+// кнопка назад
 
 function showPreviousSlide() {
     slideIndex = (slideIndex - 1 + slideCount) % slideCount;
     updateSlider();
+    ChangeSt()
 }
 
-//делаем кнопку в перёд
+// кнопка в перёд
 
-function showNextSlide () {
-    slideIndex = (slideIndex + 1 ) % slideCount;
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
     updateSlider();
+    ChangeSt()
 }
 
 //функция для обновления слайда
 
-function updateSlider () {
-    slides.forEach((slide, index)=>{
-        if(index === slideIndex){
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
             slide.style.display = 'flex';
-        } else{
+        } else {
             slide.style.display = 'none';
         }
     }
-   
-)};
+    )
+};
 
 //код для работы переключения по точкам
 
 dots.forEach(dot => {
     dot.addEventListener('click', (e) => {
         slideIndex = parseInt(e.target.dataset.index);
+        ChangeSt();
         updateSlider(slideIndex);
+    });
+});
+
+//Код для переключения стиля у dot
+
+function ChangeSt() {
+    dots.forEach((dot, index) => {
+        if (index === slideIndex) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');         
+        }
+    }
+)
+};
+
+//код для работы переключения по городам
+
+city.forEach(main_city_pacciv =>{
+    main_city_pacciv.addEventListener('click',(e) =>{
+        slideIndex = parseInt(e.target.dataset.index);
+        updateSlider(slideIndex);
+        ChangeSt();
     });
 });
 
